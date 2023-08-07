fa9d35cc5c31df753aae90da575d3949fef0c66a
MrPrakashR <77004328+MrPrakashR@users.noreply.github.com>
2023-08-07 11:21:12 +0000

    OCR Discord Bot Creation

---
+++ index.js
@@ -0,0 +1,27 @@
+const { Client, GatewayIntentBits } = require("discord.js")
+require("discord-reply");
+const Tesseract = require("tesseract.js");
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds,GatewayIntentBits.GuildMessages,GatewayIntentBits.MessageContent] });
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate',(message) => {
+    if(message.attachments.size > 0){
+        message.attachments.forEach(attachment => {
+          console.log(attachment.proxyURL)
+          var ImageURL = attachment.proxyURL;
+          Tesseract.recognize(
+            ImageURL,
+            "eng",
+            ).then(({ data: { text } }) => {
+                message.reply(text);
+              })
+        });
+      }
+})
+
+
+client.login("TOKEN")
